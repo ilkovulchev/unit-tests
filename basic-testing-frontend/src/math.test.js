7482c73f539364ac3d408b13f27eb1266162f8d6
ilko <ilko.valchev94@gmail.com>
2024-02-01 16:03:48 +0200

    test

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -1,8 +1,38 @@
-import { expect, it } from "vitest";
+import { expect, it, describe } from "vitest";
 import { add } from "./math";
 
-it("Should summarize all number values in an array", () => {
-  const result = add([1, 2, 3]);
+describe("add()", () => {
+  it("Should summarize all number values in an array", () => {
+    const result = add([1, 2, 3]);
 
-  expect(result).toBe(6);
+    expect(result).toBe(6);
+  });
+
+  it("Should yield NaN if at least one invalid number is provided", () => {
+    const result = add([1, 2, "invalid"]);
+
+    expect(result).toBeNaN();
+  });
+});
+
+describe("add2()", () => {
+  it("Should yield a correct sym f an array of numeric string values is provided", () => {
+    const result = add(["1", "2", "3"]);
+
+    expect(result).toBe(6);
+  });
+
+  it("Should throw an error if no value is passed into the function", () => {
+    const result = () => {
+      add();
+    };
+    expect(result).toThrow();
+  });
+
+  it("Should throw an error if provided with multiple arguments instead of an array", () => {
+    const result = () => {
+      add(1, 2, 3);
+    };
+    expect(result).toThrow();
+  });
 });
